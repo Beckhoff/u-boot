c89ebb98612dc92bc5bd801e6abc9d0cae48ecb0
Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
2025-12-15 08:32:30 +0100

    bhf: add ci and debian package build
    This is a squashed version of all the packaging / ci related changes
    from our bhf/v2024.10 branch. For git history look there.
    
    We create a debian package for our CX8200 / 9240 devices that extracts
    the u-boot files and fpga images to our efi partition. Since the efi is
    fat, apt won't install there directly and we copy the files.
    
    We also add a service to initialize the rtc configuration to use the
    battery.

---
+++ .ci/build-zynqmp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+set -u
+
+device=${1}
+script_path="$(cd "$(dirname "${0}")" && pwd)"
+readonly script_path
+
+# get pmufw and arm-trusted-firmware
+curl -L -o pmufw.bin 'https://git.beckhoff.dev/beckhoff/zynqmp-pmufw-builder/-/jobs/2176082/artifacts/raw/pmufw.bin'
+curl -L -o bl31.bin 'https://git.beckhoff.dev/beckhoff/arm-trusted-firmware/-/jobs/754226/artifacts/raw/build/zynqmp/release/bl31.bin'
+
+sha256sum -c "${script_path}/pmufw.bin.sha256sum"
+sha256sum -c "${script_path}/bl31.bin.sha256sum"
+
+# convert PMU config object
+tools/zynqmp_pm_cfg_obj_convert.py "board/beckhoff/${device}/pm_cfg_obj.c" pmu_obj.bin
+
+export BL31=bl31.bin
+export BINMAN_ALLOW_MISSING=1
+make CROSS_COMPILE=aarch64-linux-gnu- clean mrproper
+make CROSS_COMPILE=aarch64-linux-gnu- "beckhoff_${device}_defconfig"
+make CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
+
+mkdir -p "build/${device}"
+cp spl/boot.bin u-boot.itb "build/${device}/"
