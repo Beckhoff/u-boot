8469ed4eaa5c291724a293d9be72d934ee9ef22d
Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
2025-12-17 09:04:17 +0100

    board/beckhoff/zynqmp: load fpga bitstreams
    fpga bitstream is loaded based on factory data flash. For differenc
    connected io interfaces different fpga bitstreams are required. We
    provide a fallback path to load when the flash isn't programmed yet.

--- include/configs/beckhoff_zynqmp_cx9240.h
+++ include/configs/beckhoff_zynqmp_cx9240.h
@@ -7,11 +7,17 @@
 #define __CONFIG_BECKHOFF_CX9240_H
 
 #include <configs/xilinx_zynqmp.h>
+#include <configs/beckhoff_zynqmp.h>
 
 #undef CFG_EXTRA_ENV_SETTINGS
 #define CFG_EXTRA_ENV_SETTINGS \
 	ENV_MEM_LAYOUT_SETTINGS \
 	BOOTENV \
+	BECKHOFF_ZYNQMP_LOAD_FPGA \
 	"fdt_addr_r=0x28000000\0" \
+	"fpga_fallback_file=CX9240-B000-3.bin\0" \
+	"preboot=" \
+	"run load_fpga_bitstream;" \
+	"\0"
 
 #endif /* __CONFIG_BECKHOFF_CX9240_H */
