224b80edd7dcdb6015be81c1fb5ae9e74dae9387
Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
2025-12-17 09:04:17 +0100

    board/beckhoff/zynqmp: load fpga bitstreams
    fpga bitstream is loaded based on factory data flash. For differenc
    connected io interfaces different fpga bitstreams are required. We
    provide a fallback path to load when the flash isn't programmed yet.

---
+++ include/configs/beckhoff_zynqmp.h
@@ -0,0 +1,52 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) Beckhoff Automation GmbH & Co. KG
+ */
+
+#define BECKHOFF_ZYNQMP_LOAD_FPGA \
+	"try_fpga_load=" \
+		"fpga_path=${fpga_prefix}${fpga_file}.gz;" \
+		"echo \"looking for fpga bitstream ${fpga_path}\";" \
+		"if test ! -e mmc 0:1 ${fpga_path}; then;" \
+			"echo \"fpga bitstream ${fpga_path} not found\";" \
+			"fpga_loaded=0;" \
+			"exit;" \
+		"fi;" \
+		"load mmc 0 ${loadaddr} ${fpga_path};" \
+		"if test $? -ne 0; then;" \
+			"echo \"loading bitstream ${fpga_path} to ${loadaddr} failed\";" \
+			"fpga_loaded=0;" \
+			"exit;" \
+		"fi;" \
+		"setexpr loadaddr_unzipped ${loadaddr} + ${filesize};" \
+		"unzip ${loadaddr} ${loadaddr_unzipped};" \
+		"if test $? -ne 0; then;" \
+			"echo \"unzip bitstream ${fpga_path} failed\";" \
+			"fpga_loaded=0;" \
+			"exit;" \
+		"fi;" \
+		"fpga load 0 ${loadaddr_unzipped} ${file} ${filesize};" \
+		"if test $? -ne 0; then;" \
+			"fpga_loaded=0;" \
+			"exit;" \
+		"fi;" \
+		"echo \"loaded bitstream from ${fpga_path} successfully\";" \
+		"fdt addr ${fdtcontroladdr};" \
+		"fdt set /chosen ccat0-firmware-loaded ${fpga_path};" \
+		"fpga_loaded=1;" \
+		"exit;" \
+	"\0" \
+	"load_fpga_bitstream=" \
+	"mtd read beckhoff_factory_data ${loadaddr} 0xc4 0x20;" \
+	"setexpr termination_addr ${loadaddr} + 0x20;" \
+	"mw.b ${termination_addr} 0x00;" \
+	"setexpr.s mtd_fpga_file *${loadaddr};" \
+	"fpga_prefix=\"beckhoff/fpga/\";" \
+	"fpga_file=${mtd_fpga_file};" \
+	"fpga_loaded=0;" \
+	"run try_fpga_load;" \
+	"if test ${fpga_loaded} -ne 1; then;" \
+		"fpga_file=${fpga_fallback_file};" \
+		"run try_fpga_load;" \
+	"fi;" \
+	"\0"
--- include/configs/beckhoff_zynqmp_cx8200.h
+++ include/configs/beckhoff_zynqmp_cx8200.h
@@ -7,11 +7,17 @@
 #define __CONFIG_BECKHOFF_CX8200_H
 
 #include <configs/xilinx_zynqmp.h>
+#include <configs/beckhoff_zynqmp.h>
 
 #undef CFG_EXTRA_ENV_SETTINGS
 #define CFG_EXTRA_ENV_SETTINGS \
 	ENV_MEM_LAYOUT_SETTINGS \
 	BOOTENV \
+	BECKHOFF_ZYNQMP_LOAD_FPGA \
 	"fdt_addr_r=0x28000000\0" \
+	"fpga_fallback_file=CX8200-B000-2.bin\0" \
+	"preboot=" \
+	"run load_fpga_bitstream;" \
+	"\0"
 
 #endif /* __CONFIG_BECKHOFF_CX8200_H */
