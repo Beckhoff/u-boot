Beckhoff/u-boot

commit aa711ac815618bdc2c992c4b3869f6ef27b80202

tools: zynqmp_psu_init_minimize.sh: Switch to clang-format
Replace the use of scripts/Lindent with clang-format in
zynqmp_psu_init_minimize.sh. This change is made to align with the rest
of the codebase that uses clang-format for code formatting. This ensures
consistency across all scripts in terms of code style and formatting.

Tested-by: Michal Simek <michal.simek@amd.com>
Signed-off-by: Javier Tia <javier.tia@linaro.org>

tools/zynqmp_psu_init_minimize.sh
@@ -82,7 +82,7 @@ for i in $FUNCS_TO_REMOVE; do
 sed -i "/$i/,/^}$/d" ${OUT}
 done
 
-scripts/Lindent ${OUT}
+clang-format -i -style=file ${OUT}
 
 # Prepend 'static' to internal functions
 sed -i 's/^.*data(void)$/static &/g' ${OUT}

commit eab48c936ab5a53f87f482e3b99872f4b5006fd3

tools: zynqmp_psu_init_minimize.sh: stop removing braces
Removing braces when already using clang format led to a broken init
peripheral function without braces.

static void init_peripheral(void)
  psu_mask_write(0xFD5F0018, 0x8000001FU, 0x8000001FU);

There don't seem to be other places where braces are removed when
already using clang-format, so we can just drop it entirely.

Fixes: aa711ac81561 ("tools: zynqmp_psu_init_minimize.sh: Switch to clang-format")
Signed-off-by: Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>

tools/zynqmp_psu_init_minimize.sh
@@ -133,12 +133,6 @@ sed -i -e 's|\r\r}\r|\r}\r|g' ${TMP}
 # Remove empty lines after '{' line
 sed -i -e 's| {\r\r| {\r|g' ${TMP}
 
-# Remove braces {} around single statement blocks. E.g.:
-# | while (e) { |    | while (e) |
-# |     stg();  | => |     stg();|
-# | }           |
-sed -i -r 's| \{(\r[^\r]*;)\r\t*\}|\1|g' ${TMP}
-
 # Remove Unnecessary parentheses around 'n_code <= 0x3C' and similar. E.g.:
 # if ((p_code >= 0x26) && ...) -> if (p_code >= 0x26 && ...)
 sed -i -r 's|\((._code .= [x[:xdigit:]]+)\)|\1|g' ${TMP}