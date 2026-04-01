77e02b7b57b60f20876d8f5f0710318458da11a2
Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
2026-04-01 13:11:48 +0000

    bhf: build-zynqmp: refactor to POSIX style
    style guide: export after assignment, _ prefixed locals, usage()
    function, brace-delimited variables.

--- .ci/build-zynqmp.sh
+++ .ci/build-zynqmp.sh
@@ -1,26 +1,51 @@
-#!/bin/bash
+#!/bin/sh
+# SPDX-License-Identifier: MIT
+# Copyright (C) Beckhoff Automation GmbH & Co. KG
+
+usage() {
+	cat <<- EOF
+		Build script for Beckhoff ZynqMP u-boot targets.
+
+		USAGE:
+			${0##*/} <device>
+
+		ARGUMENTS:
+			device    Device name (cx8200, cx9240)
+
+		EXAMPLES:
+			${0##*/} cx8200
+			${0##*/} cx9240
+
+	EOF
+}
+
 set -e
 set -u
 
-device=${1}
+_device="${1:?Missing device argument
+$(usage)}"
+readonly _device
+
 script_path="$(cd "$(dirname "${0}")" && pwd)"
 readonly script_path
 
-# get pmufw and arm-trusted-firmware
-curl -L -o pmufw.bin 'https://git.beckhoff.dev/beckhoff/zynqmp-pmufw-builder/-/jobs/2176082/artifacts/raw/pmufw.bin'
-curl -L -o bl31.bin 'https://git.beckhoff.dev/beckhoff/arm-trusted-firmware/-/jobs/754226/artifacts/raw/build/zynqmp/release/bl31.bin'
+curl --location --output pmufw.bin \
+	'https://git.beckhoff.dev/beckhoff/zynqmp-pmufw-builder/-/jobs/2176082/artifacts/raw/pmufw.bin'
+curl --location --output bl31.bin \
+	'https://git.beckhoff.dev/beckhoff/arm-trusted-firmware/-/jobs/754226/artifacts/raw/build/zynqmp/release/bl31.bin'
 
-sha256sum -c "${script_path}/pmufw.bin.sha256sum"
-sha256sum -c "${script_path}/bl31.bin.sha256sum"
+sha256sum --check "${script_path}/pmufw.bin.sha256sum"
+sha256sum --check "${script_path}/bl31.bin.sha256sum"
 
-# convert PMU config object
-tools/zynqmp_pm_cfg_obj_convert.py "board/beckhoff/${device}/pm_cfg_obj.c" pmu_obj.bin
+tools/zynqmp_pm_cfg_obj_convert.py "board/beckhoff/${_device}/pm_cfg_obj.c" pmu_obj.bin
 
-export BL31=bl31.bin
-export BINMAN_ALLOW_MISSING=1
+BL31=bl31.bin
+export BL31
+BINMAN_ALLOW_MISSING=1
+export BINMAN_ALLOW_MISSING
 make CROSS_COMPILE=aarch64-linux-gnu- clean mrproper
-make CROSS_COMPILE=aarch64-linux-gnu- "beckhoff_${device}_defconfig"
-make CROSS_COMPILE=aarch64-linux-gnu- -j$(nproc)
+make CROSS_COMPILE=aarch64-linux-gnu- "beckhoff_${_device}_defconfig"
+make CROSS_COMPILE=aarch64-linux-gnu- --jobs="$(nproc)"
 
-mkdir -p "build/${device}"
-cp spl/boot.bin u-boot.itb "build/${device}/"
+mkdir --parents "build/${_device}"
+cp spl/boot.bin u-boot.itb "build/${_device}/"
