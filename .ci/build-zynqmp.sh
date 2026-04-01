16b8715fcd4ce9ebce1da7da66e577d7993b45e6
Steffen Dirkwinkel <s.dirkwinkel@beckhoff.com>
2026-04-01 13:11:48 +0000

    bhf: build-zynqmp: add FPGA support and simplify debian rules

--- .ci/build-zynqmp.sh
+++ .ci/build-zynqmp.sh
@@ -8,15 +8,18 @@ usage() {
 
 		USAGE:
 			${0##*/} build <device>
+			${0##*/} download
 			${0##*/} update-checksums
 
 		COMMANDS:
 			build               Build u-boot for device (cx8200, cx9240)
-			update-checksums    Update sha256sum files from current files on disk
+			download            Download FPGA packages, firmware, and update psu_init files
+			update-checksums    Update all sha256sum files from current files on disk
 
 		EXAMPLES:
 			${0##*/} build cx8200
 			${0##*/} build cx9240
+			${0##*/} download
 			${0##*/} update-checksums
 
 	EOF
@@ -47,9 +50,44 @@ update_checksum() {
 	printf 'Updated %s\n' "${_sha256}" >&2
 }
 
-build_device() {
-	local _device
-	_device="${1:?Missing device}"
+update_fpga_checksums() {
+	find fpga -type f -name "*.bin.gz" -exec sha256sum {} + \
+		| LC_ALL=C sort > "${script_path}/fpga.sha256sum"
+	printf 'Updated %s\n' "${script_path}/fpga.sha256sum" >&2
+}
+
+download_binaries() {
+	if sha256sum --check --status "${script_path}/fpga.sha256sum" 2> /dev/null; then
+		printf 'FPGA binaries: checksums OK, skipping download\n' >&2
+	else
+		: "${AZDEVOPS_PAT_RO=}"
+		AZDEVOPS_PAT_RO="${AZDEVOPS_PAT_RO}" nugetctl install \
+			Beckhoff.HWE.FPGA.EmbeddedControl.CX8200.1 -OutputDirectory fpga/ -Version 2.0.0-202411261448
+		AZDEVOPS_PAT_RO="${AZDEVOPS_PAT_RO}" nugetctl install \
+			Beckhoff.HWE.FPGA.EmbeddedControl.CX8200.2 -OutputDirectory fpga/ -Version 2.2.2-202509261031
+		AZDEVOPS_PAT_RO="${AZDEVOPS_PAT_RO}" nugetctl install \
+			Beckhoff.HWE.FPGA.EmbeddedControl.CX9240.2 -OutputDirectory fpga/ -Version 2.0.0-202411281238
+		AZDEVOPS_PAT_RO="${AZDEVOPS_PAT_RO}" nugetctl install \
+			Beckhoff.HWE.FPGA.EmbeddedControl.CX9240.3 -OutputDirectory fpga/ -Version 2.4.0-202509261117
+
+		find ./fpga/Beckhoff.HWE.FPGA.EmbeddedControl.CX8200.2/ \
+			-type f -name "*.bin" ! -name "*-2.bin" \
+			-exec sh -xuc 'mv "$1" "${1%.bin}-2.bin"' _ {} \;
+		find ./fpga/Beckhoff.HWE.FPGA.EmbeddedControl.CX9240.3/ \
+			-type f -name "*.bin" ! -name "*-3.bin" \
+			-exec sh -xuc 'mv "$1" "${1%.bin}-3.bin"' _ {} \;
+
+		find ./fpga -type f -name "*.bin" -exec gzip --no-name {} \;
+
+		"${script_path}/psu-init-helper.sh" update cx8200 \
+			./fpga/Beckhoff.HWE.FPGA.EmbeddedControl.CX8200.2/SDK/CX8200.xsa
+		"${script_path}/psu-init-helper.sh" update cx9240 \
+			./fpga/Beckhoff.HWE.FPGA.EmbeddedControl.CX9240.3/SDK/CX9240.xsa
+
+		find ./fpga -name "*.nupkg" -delete
+
+		sha256sum --check "${script_path}/fpga.sha256sum"
+	fi
 
 	download_file pmufw.bin \
 		'https://git.beckhoff.dev/beckhoff/zynqmp-pmufw-builder/-/jobs/2176082/artifacts/raw/pmufw.bin' \
@@ -57,6 +95,13 @@ build_device() {
 	download_file bl31.bin \
 		'https://git.beckhoff.dev/beckhoff/arm-trusted-firmware/-/jobs/754226/artifacts/raw/build/zynqmp/release/bl31.bin' \
 		"${script_path}/bl31.bin.sha256sum"
+}
+
+build_device() {
+	local _device
+	_device="${1:?Missing device}"
+
+	download_binaries
 
 	tools/zynqmp_pm_cfg_obj_convert.py "board/beckhoff/${_device}/pm_cfg_obj.c" pmu_obj.bin
 
@@ -76,6 +121,7 @@ update_checksums() {
 	printf 'Updating checksums...\n' >&2
 	update_checksum pmufw.bin "${script_path}/pmufw.bin.sha256sum"
 	update_checksum bl31.bin "${script_path}/bl31.bin.sha256sum"
+	update_fpga_checksums
 }
 
 set -e
@@ -87,6 +133,9 @@ readonly script_path
 case "${1:-}" in
 	build)
 		build_device "${2:?Missing device argument}"
+		;;
+	download)
+		download_binaries
 		;;
 	update-checksums)
 		update_checksums
